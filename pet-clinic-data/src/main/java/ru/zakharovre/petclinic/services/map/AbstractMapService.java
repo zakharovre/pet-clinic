3de4891aeeffd70875c3127f57b2968da96115e5
Roman Zakharov <Roman_Zakharov1@epam.com>
2021-11-03 23:06:05 +0300

    implement autoIndexing

--- pet-clinic-data/src/main/java/ru/zakharovre/petclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/ru/zakharovre/petclinic/services/map/AbstractMapService.java
@@ -1,13 +1,16 @@
 package ru.zakharovre.petclinic.services.map;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
+import ru.zakharovre.petclinic.models.BaseEntity;
 
-public abstract class AbstractMapService<E, ID> {
+public abstract class AbstractMapService<E extends BaseEntity, ID extends Long> {
 
-    protected Map<ID, E> map = new HashMap<>();
+    protected Map<Long, E> map = new HashMap<>();
 
     Set<E> findAll() {
         return new HashSet<>(map.values());
@@ -17,8 +20,16 @@ public abstract class AbstractMapService<E, ID> {
         return map.get(id);
     }
 
-    E save(ID id, E entity) {
-        return map.put(id, entity);
+    E save(E entity) {
+        if(entity != null) {
+            if(entity.getId() == null) {
+                entity.setId(getNextId());
+            }
+            map.put(entity.getId(), entity);
+        } else {
+            throw new RuntimeException("Entity cannot be null");
+        }
+        return entity;
     }
 
     void deleteById(ID id) {
@@ -27,5 +38,15 @@ public abstract class AbstractMapService<E, ID> {
 
     void delete(E entity) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(entity));
+    }
+
+    private Long getNextId() {
+        Long nextId;
+        try{
+            nextId = Collections.max(map.keySet()) + 1L;
+        } catch (NoSuchElementException e) {
+            nextId = 1L;
+        }
+        return nextId;
     }
 }
--- pet-clinic-web/src/main/java/ru/zakharovre/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/ru/zakharovre/petclinic/bootstrap/DataLoader.java
@@ -6,8 +6,6 @@ import ru.zakharovre.petclinic.models.Owner;
 import ru.zakharovre.petclinic.models.Vet;
 import ru.zakharovre.petclinic.services.OwnerService;
 import ru.zakharovre.petclinic.services.VetService;
-import ru.zakharovre.petclinic.services.map.OwnerServiceMap;
-import ru.zakharovre.petclinic.services.map.VetServiceMap;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -24,13 +22,11 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Kek");
         owner1.setLastName("Lol");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Animal");
         owner2.setLastName("Lover");
         ownerService.save(owner2);
@@ -38,13 +34,11 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Owners have been loaded");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Hannibal");
         vet1.setLastName("Lecter");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Steve");
         vet2.setLastName("Jobs");
         vetService.save(vet2);
