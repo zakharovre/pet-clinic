3996d121072c4e8e09e9d566c702ae21d2dd93c3
Roman Zakharov <Roman_Zakharov1@epam.com>
2021-11-03 22:38:48 +0300

    implement findAll method in OwnerController and index

--- pet-clinic-web/src/main/java/ru/zakharovre/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/ru/zakharovre/petclinic/controllers/OwnerController.java
@@ -1,16 +1,25 @@
 package ru.zakharovre.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.zakharovre.petclinic.services.OwnerService;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
-    @GetMapping
-    public String ownerList() {
+    private final OwnerService ownerService;
 
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @GetMapping
+    public String ownerList(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
