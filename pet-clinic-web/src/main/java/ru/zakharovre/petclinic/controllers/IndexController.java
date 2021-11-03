f76d071416f067b69f3254c1a841fb8fe38ea894
Roman Zakharov <Roman_Zakharov1@epam.com>
2021-11-03 18:19:49 +0300

    add IndexController and index.html

---
+++ pet-clinic-web/src/main/java/ru/zakharovre/petclinic/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package ru.zakharovre.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+}
