daa2da94e0b69215be0df012caf766d483825be4
Roman Zakharov <Roman_Zakharov1@epam.com>
2021-11-05 17:10:46 +0300

    add SpecialityServiceMap

---
+++ pet-clinic-data/src/main/java/ru/zakharovre/petclinic/services/SpecialityService.java
@@ -0,0 +1,7 @@
+package ru.zakharovre.petclinic.services;
+
+import ru.zakharovre.petclinic.models.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/ru/zakharovre/petclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,35 @@
+package ru.zakharovre.petclinic.services.map;
+
+import java.util.Set;
+import org.springframework.stereotype.Service;
+import ru.zakharovre.petclinic.models.Speciality;
+import ru.zakharovre.petclinic.services.SpecialityService;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality entity) {
+        super.delete(entity);
+    }
+
+    @Override
+    public Speciality save(Speciality entity) {
+        return super.save(entity);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findByID(id);
+    }
+}
