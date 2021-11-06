97efdff3959f70845049f1223484ba97e35adc74
Roman Zakharov <Roman_Zakharov1@epam.com>
2021-11-06 15:24:55 +0300

    turn pojos to jpa entities

--- pet-clinic-data/src/main/java/ru/zakharovre/petclinic/models/Vet.java
+++ pet-clinic-data/src/main/java/ru/zakharovre/petclinic/models/Vet.java
@@ -2,9 +2,19 @@ package ru.zakharovre.petclinic.models;
 
 import java.util.HashSet;
 import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vets_specialities", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
